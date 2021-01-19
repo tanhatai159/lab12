553590ec1008ddaf8264cde212eadb2bd5e7e268
salaza <gun159@outlook.com>
2021-01-19 18:37:24 +0700

    completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -17,3 +17,26 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+    double sd = 0 ,gm = 1,hm = 0;
+    double max = A[0], min = A[0];
+    for(int i = 0;i < N;i++){
+        B[0] += A[i];
+        sd += pow(A[i],2);
+        gm *= pow(A[i],1/double(N));
+        hm += 1/A[i];
+        if(A[i] > max){
+            max = A[i];
+        }
+        if(A[i] < min){
+            min = A[i];
+        }
+    }
+    B[0] = B[0]/N;
+    B[1] = sqrt((sd/N)-pow(B[0],2));
+    B[2] = gm;
+    B[3] = N/hm;
+    B[4] = max;
+    B[5] = min;
+}
--- lab12_2.cpp
+++ lab12_2.cpp
@@ -21,3 +21,36 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool image[][M]){
+    for(int i = 0;i < 72;i++){
+        cout << "-";
+    }
+    cout << endl;
+    for(int i = 0;i < N;i++){
+        cout << "|";
+        for(int j = 0;j < 70;j++){
+            if(image[i][j] == 1){
+                cout << "*";
+            }
+            else{
+                cout << " ";
+            }
+        }
+        cout << "|\n";
+    }
+    for(int i = 0;i < 72;i++){
+        cout << "-";
+    }
+    cout << endl;    
+}
+
+void updateImage(bool image[][M],int s,int x,int y){
+    for(int i = 0;i < N;i++){
+        for(int j = 0;j < M;j++){
+            if(s-1 >= sqrt(pow(i-x,2)+pow(j-y,2))){
+                image[i][j] = 1;
+            }
+        }
+    }
+}
